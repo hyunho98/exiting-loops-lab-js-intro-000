535095e0f7405a08b623496571c1be56ae4856db
Alex Lee <hyunho98@gmail.com>
2019-05-13 23:01:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ breakOut(array, changeValue, stopValue) {
     array[i] = changeValue;
   }
 }
+
+keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+}
+
+findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == findFn()) {
+      return array[i];
+    }
+  }
+  return null;
+}
